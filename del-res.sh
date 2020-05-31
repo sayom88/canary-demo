f6c7880283cc809a517e70de5c75b6feb2b16da2
vinamra28 <vinjain@redhat.com>
2020-05-31 19:18:06 +0530

    Removed extra comments

--- del-res.sh
+++ del-res.sh
@@ -6,6 +6,7 @@ $command delete configmap deployment
 $command delete configmap istio
 $command delete -f build-deploy.yaml
 $command delete -f canary-deploy.yaml
+$command delete -f https://raw.githubusercontent.com/tektoncd/catalog/v1beta1/git/git-clone.yaml
 $command delete -f canary-test.yaml
 $command delete -f pipeline.yaml
 $command delete -f pipeline-run.yaml
